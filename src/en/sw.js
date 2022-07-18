308b8699b1bffdae3fcab1e6cfd360f2df2b7540
marmooo <marmooo@users.noreply.github.com>
2022-07-19 00:32:16 +0900

    bump signature_pad from 4.0.5 to 4.0.6

--- src/en/sw.js
+++ src/en/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = "2022-06-18 22:35";
+var CACHE_NAME = "2022-07-19 00:32";
 var urlsToCache = [
   "/emoji-fill-hole/",
   "/emoji-fill-hole/index.js",
--- src/ja/sw.js
+++ src/ja/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = "2022-07-02 15:10";
+var CACHE_NAME = "2022-07-19 00:32";
 var urlsToCache = [
   "/emoji-fill-hole/",
   "/emoji-fill-hole/index.js",
