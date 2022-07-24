e07f56259afef658ea0e5185803e2538e184a078
marmooo <marmooo@users.noreply.github.com>
2022-07-24 09:06:20 +0900

    bump bootstrap from 5.1.3 to 5.2.0

--- src/en/sw.js
+++ src/en/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = "2022-07-23 00:09";
+var CACHE_NAME = "2022-07-24 00:46";
 var urlsToCache = [
   "/emoji-fill-hole/",
   "/emoji-fill-hole/index.js",
@@ -12,7 +12,7 @@ var urlsToCache = [
   "/emoji-fill-hole/mp3/end.mp3",
   "/emoji-fill-hole/mp3/correct3.mp3",
   "/emoji-fill-hole/favicon/favicon.svg",
-  "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css",
   "https://cdn.jsdelivr.net/npm/signature_pad@4.0.4/dist/signature_pad.umd.min.js",
   "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@3.17.0/dist/tf.min.js",
   "https://fonts.googleapis.com/css?family=Source+Code+Pro",
