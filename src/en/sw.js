fab85db0cc6c233c6a8d3bfd3f4d316f3419ef4a
marmooo <marmooo@users.noreply.github.com>
2022-10-09 10:18:54 +0900

    bump tfjs from 3.20.0 to 3.21.0

--- src/en/sw.js
+++ src/en/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = "2022-10-08 11:03";
+var CACHE_NAME = "2022-10-09 10:18";
 var urlsToCache = [
   "/emoji-fill-hole/",
   "/emoji-fill-hole/index.js",
@@ -14,7 +14,7 @@ var urlsToCache = [
   "/emoji-fill-hole/favicon/favicon.svg",
   "https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css",
   "https://cdn.jsdelivr.net/npm/signature_pad@4.0.9/dist/signature_pad.umd.min.js",
-  "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@3.20.0/dist/tf.min.js",
+  "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@3.21.0/dist/tf.min.js",
   "https://fonts.googleapis.com/css?family=Source+Code+Pro",
 ];
 
