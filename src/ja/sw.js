89b8925cfa392638db5a5726aae5e23bfdb3733f
marmooo <marmooo@users.noreply.github.com>
2023-02-23 11:10:20 +0900

    bump signature_pad from 4.1.4 to 4.1.5

--- src/ja/sw.js
+++ src/ja/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = "2023-01-09 17:45";
+var CACHE_NAME = "2023-02-23 11:10";
 var urlsToCache = [
   "/emoji-fill-hole/",
   "/emoji-fill-hole/index.js",
@@ -15,7 +15,7 @@ var urlsToCache = [
   "/emoji-fill-hole/favicon/favicon.svg",
   "https://marmooo.github.io/yomico/yomico.min.js",
   "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css",
-  "https://cdn.jsdelivr.net/npm/signature_pad@4.1.4/dist/signature_pad.umd.min.js",
+  "https://cdn.jsdelivr.net/npm/signature_pad@4.1.5/dist/signature_pad.umd.min.js",
   "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@4.2.0/dist/tf.min.js",
   "https://fonts.googleapis.com/css?family=Source+Code+Pro",
 ];
