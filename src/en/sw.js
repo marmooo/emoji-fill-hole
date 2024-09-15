bb75dea5981a3e4b36378f3f836998c23b6580fb
marmooo <marmooo@users.noreply.github.com>
2024-09-15 13:28:21 +0900

    bump tfjs from 4.20.0 to 4.21.0

--- src/en/sw.js
+++ src/en/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "2024-08-31 10:16";
+const CACHE_NAME = "2024-09-15 13:27";
 const urlsToCache = [
   "/emoji-fill-hole/",
   "/emoji-fill-hole/index.js",
@@ -10,7 +10,7 @@ const urlsToCache = [
   "/emoji-fill-hole/mp3/end.mp3",
   "/emoji-fill-hole/mp3/correct3.mp3",
   "/emoji-fill-hole/favicon/favicon.svg",
-  "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@4.20.0/dist/tf.min.js",
+  "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@4.21.0/dist/tf.min.js",
   "https://fonts.googleapis.com/css?family=Source+Code+Pro",
 ];
 
